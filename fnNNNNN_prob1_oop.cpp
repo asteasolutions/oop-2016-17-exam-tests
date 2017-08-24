1f3e1edd7666c56e9f82594f84939919a0af0986
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2017-08-24 14:21:09 +0300

    Примерно решение на първи вариант за ниво 3.

--- fnNNNNN_prob1_oop.cpp
+++ fnNNNNN_prob1_oop.cpp
@@ -6,12 +6,100 @@
 // Date: 25.08.2017
 //
 
+#include <cstring>
+
 ///
 /// Клас представящ дата
 ///
 class Date
 {
-  // моля, попълнете реализацията на клас тук
+private:
+	int m_Day;
+	int m_Month;
+	int m_Year;
+
+public:
+
+	Date(int Day = 0, int Month = 0, int Year = 0) :
+		m_Day(Day),
+		m_Month(Month),
+		m_Year(Year)
+    {
+      bool isValid = true;
+      
+      if (m_Day < 1 || m_Day > 31)
+        isValid = false;
+
+      if (m_Day == 31)
+      {
+        switch (m_Month) {
+        case 4:
+        case 6:
+        case 9:
+        case 11: isValid = false;
+        }
+      }
+
+      if (isValid && m_Month == 2)
+      {
+        bool isLeap = (m_Year % 4 == 0 && m_Year % 100 != 0) || m_Year % 400 == 0;
+        isValid = m_Day <= 28 || isLeap && m_Day == 29;
+      }
+
+      if (!isValid)
+      {
+        m_Day = m_Month = m_Year = 0;
+      }
+    }
+
+    int GetDay() const
+    {
+		return m_Day;
+    }
+
+    int GetMonth() const
+    {
+		return m_Month;
+    }
+
+    int GetYear() const
+    {
+		return m_Year;
+    }
+
+    bool operator==(const Date& other) const
+    {
+		return m_Day == other.m_Day &&
+			   m_Month == other.m_Month &&
+			   m_Year == other.m_Year;
+    }
+
+    bool operator!=(const Date& other) const
+    {
+		return !(*this == other);
+    }
+
+    bool operator< (const Date& other) const
+    {
+		return m_Year < other.m_Year ||
+			(m_Year == other.m_Year && m_Month < other.m_Month) ||
+			(m_Year == other.m_Year && m_Month == other.m_Month && m_Day < other.m_Day);
+    }
+
+    bool operator> (const Date& other) const
+    {
+		return other < *this;
+    }
+
+    bool operator<= (const Date& other) const
+    {
+		return *this < other || *this == other;
+    }
+
+    bool operator>= (const Date& other) const
+    {
+		return other <= *this;
+    }
 };
 
 ///
@@ -19,7 +107,73 @@ class Date
 ///
 class Student
 {
-  // моля, попълнете реализацията на клас тук
+private:
+    char* m_pName;
+
+    Date m_DateOfBirth;
+
+public:
+	Student() :
+		m_pName(nullptr),
+		m_DateOfBirth(1, 1, 2017)
+	{
+		SetName("");
+	}
+
+    Student(const char* pName, const Date & DateOfBirth) :
+		m_pName(nullptr),
+		m_DateOfBirth(DateOfBirth)
+    {
+		SetName(pName);
+    }
+
+	Student(const Student& other) :
+		m_pName(nullptr),
+		m_DateOfBirth(other.m_DateOfBirth)
+	{
+		SetName(other.GetName());
+	}
+
+	~Student()
+	{
+		delete m_pName;
+	}
+
+	Student & operator=(const Student & other)
+	{
+		if (this != &other)
+		{
+			SetName(other.GetName());
+			m_DateOfBirth = other.m_DateOfBirth;
+		}
+
+		return *this;
+	}
+
+	void SetName(const char* pName)
+	{
+		delete[] m_pName;
+		m_pName = new char[strlen(pName) + 1];
+		strcpy(m_pName, pName);
+	}
+
+	const char* GetName() const
+	{
+		return m_pName;
+	}
+	
+	const Date GetBirthDate() const
+	{
+		return m_DateOfBirth;
+	}
+
+	bool operator <(const Student & other) const
+	{
+		int comp = strcmp(m_pName, other.m_pName);
+
+		return comp < 0 ||
+			   (comp == 0 && m_DateOfBirth < other.m_DateOfBirth);
+	}
 };
 
 
@@ -28,14 +182,7 @@ class Student
 
 int main() {
 
-//-------------------------------------------------------------------------
-//
-// ВАЖНО: Преди предаване на решението, моля премахнете коментара от дадения по-долу ред,
-//        за да активирате автоматичните тестове за решението си.
-//
-//-------------------------------------------------------------------------
-
-//  RunTests();
+  RunTests();
   
   return 0;
 }
